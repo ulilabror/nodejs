084f2c640f14dc5fee3e7665de33466f57007378
Your Name <you@example.com>
2020-06-08 11:22:53 +0700

    nodejs

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const ytdl = require('ytdl-core');
 const cors = require("cors");
 const express = require("express");
 const app = express();
-
+const PORT = process.env.PORT || 4000;
 app.use(cors());
 
 app.get('/',(req,res)=>{
    var url= req.query.url;
    res.header("Content-Disposition", 'attachment;\  filename='+url+'.mp4');
    ytdl(url,{format:"mp4"}).pipe(res);
-}).listen(3000,()=>console.log("ok"))
+}).listen(PORT,()=>console.log("ok"))
